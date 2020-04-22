08ab90668e9fe212270a803db7029223e8a806b2
Codegnosis <paul@codegnosis.co.uk>
2020-04-22 10:27:17 +0100

    server - configurable gas

--- server/faucet.go
+++ server/faucet.go
@@ -35,6 +35,8 @@ type FaucetConfig struct {
 	FaucetPublicUrl    string
 	FaucetCliHomeDir   string
 	GoBinDir           string
+	GasPrices          string
+	GasAdjustment      string
 }
 
 type FaucetRequest struct {
@@ -109,6 +111,8 @@ func Init() {
 	config.FaucetPublicUrl = procDotEnv("FAUCET_PUBLIC_URL")
 	config.FaucetCliHomeDir = procDotEnv("FACUET_UNDCLI_HOME")
 	config.GoBinDir = procDotEnv("GO_BIN_DIR")
+	config.GasPrices = procDotEnv("GAS_PRICES")
+	config.GasAdjustment = procDotEnv("GAS_ADJUSTMENT")
 }
 
 func StartServer() {
@@ -240,8 +244,8 @@ func faucetSendHandler(w http.ResponseWriter, req *http.Request) {
 		}
 
 		undCliCmd := fmt.Sprintf(
-			"%vundcli tx send %v %v %v%v --chain-id %v --node %v --gas auto --gas-adjustment 1.5 --gas-prices 0.025nund --memo UND_TestNet_Faucet --output json%v",
-			config.GoBinDir, config.NodeKeyName, encodedAddress, config.FaucetAmountToSend, config.FaucetDenom, config.ChainID, config.NodeRpcUrl, homeDir)
+			"%vundcli tx send %v %v %v%v --chain-id %v --node %v --gas auto --gas-adjustment %v --gas-prices %v --memo UND_TestNet_Faucet --output json%v",
+			config.GoBinDir, config.NodeKeyName, encodedAddress, config.FaucetAmountToSend, config.FaucetDenom, config.ChainID, config.NodeRpcUrl, config.GasAdjustment, config.GasPrices, homeDir)
 
 		fmt.Println(undCliCmd)
 
